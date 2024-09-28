78aac03ef5c6fe0d9ed3c0a721dc4a81b9830d41
Michael Usidamen <71311948+Mheek-frosh@users.noreply.github.com>
2024-09-28 11:32:19 +0100

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -9,8 +9,8 @@ const Hero = () => {
         <WorkStatus />
         {/* Title */}
         <div className="text-center text-[38px] sm:text-[43px] md:text-[88px] leading-[44px] md:leading-[85px] lg:leading-[94px] font-normal">
-          <h1>Tega Okorare</h1>
-          <h1 className="secondary-text">Software Engineer</h1>
+          <h1>Michael Usidamen</h1>
+          <h1 className="secondary-text">Mobile Engineer</h1>
         </div>
         {/* Info */}
         <div className="text-center font-normal text-[18px]">
@@ -22,8 +22,8 @@ const Hero = () => {
       {/* Email button */}
       <div className="flex flex-col items-center space-y-3">
         <button className="flex space-x-2 items-center rounded-3xl px-[16px] py-[12px] text-[18px] font-semibold transition">
-          <a href="mailto:contact@tegacreatives.com">
-            contact@tegacreatives.com
+          <a href="mailto:michaelusidamen@gmail.com">
+            michaelusidamen@gmail.com
           </a>
         </button>
         <div className="text-[14px] font-normal">Got something for me? 👆</div>
